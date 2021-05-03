3b61bdcfb0676c9deddfc2bc3cc6507ea3c34c56
jpneey <burato348@gmail.com>
2021-05-03 08:37:57 +0800

    Stable v1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="home">
-    <h1 class="h2 fw-bold position-relative mb-0 ps-5"><img src="../assets/pmcg-highres.png" class="pmcg" height="45" alt="teams-logo" /> &nbsp; PMC Group of Companies</h1>
+    <h1 class="h2 fw-bold position-relative mb-0 ps-5"><img src="../assets/pmc-highres.png" class="pmcg" height="45" alt="teams-logo" /> &nbsp; Progressive Medical Corporation</h1>
     <p class="text-secondary ps-5"> &nbsp; <b>{{greet}}!</b> Select an app and start you video conference easily.</p>
     <Meetings />
   </div>
